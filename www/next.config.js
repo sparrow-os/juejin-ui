78f975dcb8c8b9213533125506f98c27c6624410
张立志 <zhanglizhi@tedu.cn>
2023-11-27 10:14:00 +0800

    git ignore

--- www/next.config.js
+++ www/next.config.js
@@ -2,12 +2,12 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["http://localhost:3000"],
-  },
-  experimental: {
-    appDir: true,
-  },
-  // output: "export",
+    reactStrictMode: true,
+    images: {
+        domains: ["http://localhost:3000"],
+    },
+    experimental: {
+        appDir: true,
+    },
+    output: "export",
 };
