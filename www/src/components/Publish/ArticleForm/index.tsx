9c02bbad654b9827dfd655eee8fe810d0f7c4072
= <=>
2023-12-22 10:28:17 +0900

    fix: form valid

--- www/src/components/Publish/ArticleForm/index.tsx
+++ www/src/components/Publish/ArticleForm/index.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import {memo} from 'react';
+import React, { memo, useCallback } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,12 +6,24 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
-import {TransitionProps} from '@mui/material/transitions';
-import {TextField} from "@mui/material";
-import CategoryTree from "../CategoryTree";
-import CoverImage from "../CoverImage";
-import Tag from "../Tag";
-import {useArticleForm} from "../../../store/articleEditor";
+import { TransitionProps } from '@mui/material/transitions';
+import { TextField } from '@mui/material';
+import { FieldValues, Form, useForm } from 'react-hook-form';
+import CategoryTree from '../CategoryTree';
+import CoverImage from '../CoverImage';
+import Tag from '../Tag';
+import { useArticleForm } from '../../../store/articleEditor';
+import { valibotResolver } from '@hookform/resolvers/valibot';
+import {
+  Output,
+  ValiError,
+  array,
+  maxLength,
+  minLength,
+  number,
+  object,
+  string,
+} from 'valibot';
 
 //禁用拖动
 // function PaperComponent(props: PaperProps) {
@@ -26,48 +37,83 @@ import {useArticleForm} from "../../../store/articleEditor";
 //     );
 // }
 
+const ArticlePulishSchema = object({
+  category: string('please type in string', [
+    minLength(1, 'Please select a category'),
+  ]),
+  abstracts: string('please type in string', [
+    minLength(8, 'Please type in at least 8 charactors'),
+    maxLength(300, 'Abstracts characters should exceed 300'),
+  ]),
+});
+
+type FormData = Output<typeof ArticlePulishSchema>;
+
 const Transition = React.forwardRef(function Transition(
-    props: TransitionProps & {
-        children: React.ReactElement<any, any>;
-    },
-    ref: React.Ref<unknown>,
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
 ) {
-    return <Slide direction="left" ref={ref} {...props} />;
+  return <Slide direction="left" ref={ref} {...props} />;
 });
 
 function ArticleForm() {
-    const articleForm = useArticleForm((articleForm) => articleForm);
-    const handleClose = () => {
-        console.log(JSON.stringify(articleForm, null, 2));
-        articleForm.closeDialog();
-    };
-    return (
-        <React.Fragment>
-            <Dialog
-                open={articleForm.open}
-                scroll="body"
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={handleClose}
-                aria-describedby="article-form"
-                aria-labelledby="article-form-label"
-            >
-                <DialogTitle>{"发布文章"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="article-form">
-                        <CategoryTree/>
-                        <Tag/>
-                        <CoverImage/>
-                        <TextField multiline sx={{m: 1, width: 300}} rows={4} label="摘要" id="fullWidth"/>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>取消</Button>
-                    <Button onClick={handleClose}>确定</Button>
-                </DialogActions>
-            </Dialog>
-        </React.Fragment>
-    );
-};
+  const articleForm = useArticleForm((articleForm: FormData) => articleForm);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: valibotResolver(ArticlePulishSchema),
+  });
 
-export default memo(ArticleForm)
+  console.log('errors', errors);
+
+  const onSubmit = useCallback((values: FieldValues) => {
+    console.log('values', values);
+  }, []);
+
+  const handleClose = () => {
+    console.log(JSON.stringify(articleForm, null, 2));
+    articleForm.closeDialog();
+  };
+  return (
+    <React.Fragment>
+      <Dialog
+        open={articleForm.open}
+        scroll="body"
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="article-form"
+        aria-labelledby="article-form-label"
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogTitle>{'发布文章'}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="article-form">
+              <CategoryTree register={register} />
+              <Tag />
+              <CoverImage />
+              <TextField
+                {...register('abstracts')}
+                multiline
+                sx={{ m: 1, width: 300 }}
+                rows={4}
+                label="摘要"
+                id="fullWidth"
+              />
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>取消</Button>
+            <Button type="submit">确定</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </React.Fragment>
+  );
+}
+
+export default memo(ArticleForm);
--- www/src/components/Publish/Tag/index.tsx
+++ www/src/components/Publish/Tag/index.tsx
@@ -1,104 +1,105 @@
 import * as React from 'react';
 import {
-    Checkbox,
-    FormControl,
-    InputLabel,
-    ListItemText,
-    MenuItem,
-    OutlinedInput,
-    Select,
-    SelectChangeEvent
-} from "@mui/material";
-import {useEffect} from "react";
-import httpClient from "../../../utils/HttpClient";
-import toast, {Toaster} from "react-hot-toast";
-import {useArticleForm} from "../../../store/articleEditor";
-
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material';
+import { useEffect } from 'react';
+import httpClient from '../../../utils/HttpClient';
+import toast, { Toaster } from 'react-hot-toast';
+import { useArticleForm } from '../../../store/articleEditor';
 
 //标签的对象定义
 export interface TagItem {
-    id: number;
-    tagName: string;
+  id: number;
+  tagName: string;
 }
 
 const tagKvMap = new Map<number, TagItem>([]);
 
-
 export default function Tag() {
-    const articleForm = useArticleForm((articleForm) => articleForm);
-    //服务器返回的标签列表
-    const [tagList, setTagList] = React.useState<TagItem[]>([]);
+  const articleForm = useArticleForm((articleForm) => articleForm);
+  //服务器返回的标签列表
+  const [tagList, setTagList] = React.useState<TagItem[]>([]);
 
-    let _mounted = false;
-    useEffect(() => {
-        if (_mounted) {
-            return;
+  let _mounted = false;
+  useEffect(() => {
+    if (_mounted) {
+      return;
+    }
+    console.log('tag index effect ....');
+    const fetchData = async () => {
+      try {
+        const data: any = await httpClient.get('/tag/list');
+        const tagItems = data as TagItem[];
+        if (tagItems.length == 0) {
+          console.log('category tree is empty');
+          return;
         }
-        console.log("tag index effect ....")
-        const fetchData = async () => {
-            try {
-                const data: any = await httpClient.get('/tag/list');
-                const tagItems = data as TagItem[];
-                if (tagItems.length == 0) {
-                    console.log("category tree is empty");
-                    return;
-                }
-                tagItems.forEach((item) => {
-                    tagKvMap.set(item.id, item);
-                });
-                setTagList(tagItems);
-                console.log(tagItems);
-            } catch (error) {
-                debugger;
-                toast.error("Error " + error);
-            }
-        };
-        fetchData();
-        _mounted = true;
-    }, [])
-
-
-    const tagHandleChange = (event: SelectChangeEvent<typeof valueList>) => {
-        const {target: {value}} = event;
-        const valueList = value as number[];
-        if (valueList.length > 3) {
-            toast.error("最多只能选择三个标签");
-            return;
-        }
-        articleForm.setTagIds(valueList);
-        //https://stackoverflow.com/questions/73108589/how-to-get-selected-checkbox-value-and-id-from-the-multi-select-in-mui
-        //无法确定当前是否被 选中
+        tagItems.forEach((item) => {
+          tagKvMap.set(item.id, item);
+        });
+        setTagList(tagItems);
+        console.log(tagItems);
+      } catch (error) {
+        // debugger;
+        toast.error('Error ' + error);
+      }
     };
+    fetchData();
+    _mounted = true;
+  }, []);
 
-    const renderValue = (selected: number[]) => {
-        let values:any = [];
-        selected.forEach((tagId)=>{values.push(tagKvMap.get(tagId)?.tagName)});
-        return values.join(" ");
+  const tagHandleChange = (event: SelectChangeEvent<typeof valueList>) => {
+    const {
+      target: { value },
+    } = event;
+    const valueList = value as number[];
+    if (valueList.length > 3) {
+      toast.error('最多只能选择三个标签');
+      return;
     }
+    articleForm.setTagIds(valueList);
+    //https://stackoverflow.com/questions/73108589/how-to-get-selected-checkbox-value-and-id-from-the-multi-select-in-mui
+    //无法确定当前是否被 选中
+  };
 
-    const checked = (tagId: number): boolean => {
-        return articleForm.tagIds.indexOf(tagId) > -1;
-    }
+  const renderValue = (selected: number[]) => {
+    let values: any = [];
+    selected.forEach((tagId) => {
+      values.push(tagKvMap.get(tagId)?.tagName);
+    });
+    return values.join(' ');
+  };
 
-    return (
-        <FormControl sx={{m: 1, width: 300}}>
-            <InputLabel id="tag">标签</InputLabel>
-            <Select
-                labelId="tag"
-                id="tag"
-                multiple
-                value={articleForm.tagIds}
-                onChange={tagHandleChange}
-                input={<OutlinedInput label="标签"/>}
-                renderValue={renderValue}
-            >
-                {tagList.map((entry: TagItem) => (
-                    <MenuItem key={entry.id} value={entry.id}>
-                        <Checkbox checked={checked(entry.id)}/>
-                        <ListItemText primary={entry.tagName}/>
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>
-    );
+  const checked = (tagId: number): boolean => {
+    return articleForm.tagIds.indexOf(tagId) > -1;
+  };
+
+  return (
+    <FormControl sx={{ m: 1, width: 300 }}>
+      <InputLabel id="tag">标签</InputLabel>
+      <Select
+        labelId="tag"
+        id="tag"
+        multiple
+        value={articleForm.tagIds}
+        onChange={tagHandleChange}
+        input={<OutlinedInput label="标签" />}
+        renderValue={renderValue}
+      >
+        {tagList.map((entry: TagItem) => (
+          <MenuItem key={entry.id} value={entry.id}>
+            <Checkbox checked={checked(entry.id)} />
+            <ListItemText primary={entry.tagName} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
 }
