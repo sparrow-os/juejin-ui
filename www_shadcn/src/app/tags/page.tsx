109b95d5053f086402983608f83763ffb9503bae
zh_harry <zh_harry@163.com>
2024-08-14 02:02:52 +0800

    add uploader

--- www_shadcn/src/app/tags/page.tsx
+++ www_shadcn/src/app/tags/page.tsx
@@ -1,48 +1,55 @@
 'use client'
-import React from 'react';
-import ReactTags from "react-tag-input/types/components/ReactTags";
-import {Tag} from "react-tag-input/types/components/SingleTag";
-
+import React, { useCallback, useRef, useState } from 'react'
+import ReactTags from'react-tag-autocomplete'
+interface Tag {
+    id: number
+    name: string
+}
 export default function Page() {
-    const KEYS = {
-        ENTER: [10, 13],
-        TAB: 9,
-        BACKSPACE: 8,
-        UP_ARROW: 38,
-        DOWN_ARROW: 40,
-        ESCAPE: 27,
-        SPACE: 32,
-        COMMA: 188,
-    };
+        const [tags, setTags] = useState<Tag[]>([])
+        const [suggestions, setSuggestions] = useState<Tag[]>([
+            { id: 1, name: "Apples" },
+            { id: 2, name: "Pears" },
+            { id: 3, name: "Bananas" },
+            { id: 4, name: "Mangos" },
+            { id: 5, name: "Lemons" },
+            { id: 6, name: "Apricots" }
+        ])
+        const reactTags = useRef<any>()
+        const onDelete = useCallback((tagIndex:number) => {
+            setTags(tags.filter((_, i) => i !== tagIndex))
+        }, [tags])
+        const onAddition = useCallback((newTag:any) => {
+            let newTags: Tag[] = [...tags, newTag]
+            setTags(newTags);
+        }, [tags])
 
-    const SEPARATORS = {
-        ENTER: 'Enter',
-        TAB: 'Tab',
-        COMMA: ',',
-        SPACE: ' ',
-        SEMICOLON: ';',
-    };
-    // With labelField of `name`
-    const suggestions: Tag[] = [
-        {id: "1", key: "mango", className: "mango"},
-        {id: "2", key: "pineapple",className: "mango"},
-        {id: "3", key: "orange",className: "mango"},
-        {id: "4", key: "pear",className: "mango"}
-    ];
-    return (
-        <ReactTags placeholder="Add tags" labelField="name"
-                   allowUnique={true}
-                   autocomplete={true}
-                   allowAdditionFromPaste={true}
-            allowDeleteFromEmptyInput={true}
-            allowDragDrop={false}
-            suggestions={suggestions}
-            separators={[SEPARATORS.ENTER, SEPARATORS.COMMA]}
-            inputFieldPosition="bottom"
-            editable
-            clearAll
-            maxTags={7}></ReactTags>
-    );
+        return (
+            <ReactTags classNames={{
+                root: 'react-tags border border-gray-300 rounded-md',
+                rootFocused: 'is-focused border-blue-500 focus:border-blue-500',
+                selected: 'react-tags__selected text-gray-700 bg-gray-200 rounded-md px-2 py-1 mr-2 mb-2',
+                selectedTag: 'react-tags__selected-tag',
+                selectedTagName: 'react-tags__selected-tag-name',
+                search: 'react-tags__search',
+                searchInput: 'react-tags__search-input',
+                suggestions: 'react-tags__suggestions',
+                suggestionActive: 'is-active',
+                suggestionDisabled: 'is-disabled'
+            }}
+                ref={reactTags}
+                tags={tags}
+                suggestions={suggestions}
+                onDelete={onDelete}
+                onAddition={onAddition}
+                       onkeydown={(e:any) => {
+                if (e.key === 'Backspace' && reactTags.current.input.value === '') {
+                    e.preventDefault()
+                    onDelete(tags.length - 1)
+                }
+            }}
+            />
+        )
 };
 
 
